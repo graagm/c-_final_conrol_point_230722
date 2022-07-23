9ed458e5c0cc75bba2d55a8080cb6e1660c29ad1
graagm <graagm@gmail.com>
2022-07-23 17:58:25 +0300

    TASK with testrun

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void NewRandomArray(string [] msStart)
+{
+    int N = msStart.Length;
+    string s = string.Empty;
+
+    for(int i=0; i<N; i++)
+    {
+        int counts_of_symbols = new Random().Next(2, 9);
+        for(int j=0; j<counts_of_symbols; j++)
+        {  
+            int ch_Ascii = new Random().Next(32, 123);
+            char c = Convert.ToChar(ch_Ascii); 
+            s += c;
+        }
+        msStart[i] = s;
+        s = string.Empty;
+    }
+
+}
+
+void PrintedArray(string [] ms)
+{
+    int N = ms.Length;
+    for(int i=0; i<N; i++)
+    {
+        Console.WriteLine($"{i+1}: {ms[i]}");
+    }
+}
+
+int X = new Random().Next(10, 14);
+string [] msGlobal = new string [X];
+NewRandomArray(msGlobal);
+PrintedArray(msGlobal);
+
+string [] ms_final = new string[0];
+int index = 0;
+while(index < msGlobal.Length)
+{
+    if (msGlobal[index].Length <= 3)
+    {
+        Array.Resize(ref ms_final, ms_final.Length + 1);
+        ms_final[ms_final.Length - 1] = msGlobal[index];
+
+    }
+
+    index++;
+}
+
+PrintedArray(ms_final);
